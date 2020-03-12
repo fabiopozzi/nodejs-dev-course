3e24e4c730cf571f47adfa81a8d384564dd735b6
Fabio Pozzi <fabio@fabiopozzi.net>
2020-03-13 00:38:11 +0100

    Fourth challenge completed Use yargs to handle command line arguments. Add two new commands to app.js

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,10 +1,45 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes = require('./notes.js')
 
-const command = process.argv[2]
+// Customise yargs version
+yargs.version('1.1.0')
 
-if (command === 'add') {
-    console.log('Adding note!')
-} else if (command === 'remove') {
-    console.log('Removing note!')
-}
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function(argv) {
+        console.log('Adding a new note!')
+    }
+})
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a new note',
+    handler: function(argv) {
+        console.log('Removing the note')
+    }
+})
+
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List all notes',
+    handler: function() {
+        console.log('Listing all notes...')
+    }
+})
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function() {
+        console.log('Reading a note...')
+    }
+})
+
+
+console.log(yargs.argv)
