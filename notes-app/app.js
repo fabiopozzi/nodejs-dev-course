d6efefb64c6f2535f5e7fe6b0057da92ff2017ce
Fabio Pozzi <fabio@fabiopozzi.net>
2020-03-12 22:22:59 +0100

    First challenge completed.

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,5 @@
 const fs = require('fs')
 
-fs.writeFileSync('notes.txt', 'My name is Fabio.')
+// fs.writeFileSync('notes.txt', 'My name is Fabio.')
+
+fs.appendFileSync('notes.txt', 'I live in Italy.')
