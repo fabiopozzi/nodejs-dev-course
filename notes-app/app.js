39a084d53f8825bbd93cf45ec53d2abfec5e859e
Fabio Pozzi <fabio@fabiopozzi.net>
2020-03-13 11:35:31 +0100

    Remove note functionality implemented.

--- notes-app/app.js
+++ notes-app/app.js
@@ -32,9 +32,16 @@ yargs.command({
 // Create remove command
 yargs.command({
     command: 'remove',
-    describe: 'Remove a new note',
+    describe: 'Remove a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
     handler: function(argv) {
-        console.log('Removing the note')
+        notes.removeNote(argv.title)
     }
 })
 
