2cee5f2817c838ba22b36a0d6ff6de74d5625b47
Fabio Pozzi <fabio@fabiopozzi.net>
2020-03-25 15:23:19 +0100

    Implement the same thing on callback and promises to show the basic promises mechanism and syntax

--- playground/4-callback.js
+++ playground/4-callback.js
@@ -1,34 +1,13 @@
-// setTimeout(() => {
-//     console.log('2 seconds are up')
-// }, 2000)
-// // function passed as argument to be called later on
-
-// const names = ['Andrew', 'Jen', 'Jess']
-// const shortNames = names.filter((name) => {
-//     return name.length <= 4
-// })
-
-// const geocode = (address, callback) => {
-//     setTimeout(() => {
-//         const data = {
-//             latitude: 0,
-//             longitude: 0
-//         }
-
-//         callback(data)
-//     }, 2000)
-// }
-
-// geocode('Philadelphia', (data) => {
-//     console.log(data)
-// })
-
-const add = (n1, n2, callback) => {
-    setTimeout(() => {
-        callback(n1 + n2)
+const doWorkCallback = (callback) => {
+    setTimeout(()=> {
+        console.log('This is my error!', undefined)
     }, 2000)
 }
 
-add(1, 4, (sum) => {
-    console.log(sum) // Should print: 5
+doWorkCallback((error, result) => {
+    if (error) {
+        return console.log(error)
+    }
+
+    console.log(result)
 })
