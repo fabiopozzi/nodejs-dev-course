e535b214f9ca07af0879a2adcdaa781b05c16e8a
Fabio Pozzi <fabio@fabiopozzi.net>
2020-03-28 07:51:31 +0100

    Promises chaining, async/await syntax examples

--- playground/8-promises.js
+++ playground/8-promises.js
@@ -1,12 +1,17 @@
-const doWorkPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        // resolve([7, 4, 1])
-        reject('Things went wrong')
-    }, 2000)
-})
+const add  = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a + b)
+        }, 2000)
+    })
+}
 
-doWorkPromise.then((result) => {
-    console.log('Success!', result)
-}).catch((error) => {
-    console.log('Error!', error)
+// promise chaining
+add(1, 1).then((sum) => {
+    console.log(sum)
+    return add(sum, 4)
+}).then((sum2) => {
+    console.log(sum2)
+}).catch((e) => {
+    console.log(e)
 })
